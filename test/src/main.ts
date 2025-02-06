1f194c15a2396a69d79f25f3b39fa9ad4e828f3b
ne1410s <pgj646@gmail.com>
2025-02-06 18:46:41 +0000

    demo arrange

--- test/src/main.ts
+++ test/src/main.ts
@@ -1,2 +1,22 @@
+import { NewDemo } from '@ne1410s/new-demo';
 import './styles.scss';
-export { NewDemo } from '@ne1410s/new-demo';
+export { NewDemo };
+
+const test = document.querySelectorAll<NewDemo>('new-demo');
+console.log(test.length);
+
+const redMenu = document.createElement('new-demo');
+const redItem1 = document.createElement('li');
+const redSub1 = document.createElement('ul');
+const redSubItem1 = document.createElement('li');
+redItem1.innerText = 'Item 1 Prog';
+redSubItem1.innerText = 'Edit labels';
+redSub1.appendChild(redSubItem1);
+redItem1.appendChild(redSub1);
+redMenu.appendChild(redItem1);
+document.querySelector('.red.box')!.appendChild(redMenu);
+document.querySelectorAll('new-demo').forEach((m) => {
+  m.addEventListener('itemselect', (event) => console.log(event));
+  m.addEventListener('itemhover', (event) => console.log(event));
+  m.addEventListener('itemunhover', (event) => console.log(event));
+});
